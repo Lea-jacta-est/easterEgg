7c2e0136cdba11fcd20efba9e65d4f07d43605f5
Lea Godard <lea.godard.web@gmail.com>
2019-05-03 17:06:57 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -313,7 +313,7 @@ class App extends Component {
             <Switch>
               <Route exact path="/generique" component={Intro} />
               }
-            </Switch>
+                      </Switch>
           </div>
 
         </div>
